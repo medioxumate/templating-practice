766f24a3ad97ca5008c72c6379338f7b8c881c00
Evan Wheeler <effyn@gmx.com>
2019-04-22 16:10:16 -0400

    Add default view

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3->set('DEBUG', 3);
+
+$f3->route('GET /', function($f3) {
+    $view = new Template();
+    echo $view->render('views/view1.html');
+});
+
+$f3->run();
