9c000f84695c3b3542ffa5972a66b37d6e288538
[Brian Kiehn] <[briankiehn@comcast.net]>
2019-04-22 16:20:22 -0400

    Bwwahahaha

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
 $f3->route('GET /', function($f3) {
+    //Set a F3 variable
+
+    $f3->set('title', "Practicing with Templates");
+
     $view = new Template();
     echo $view->render('views/view1.html');
 });
